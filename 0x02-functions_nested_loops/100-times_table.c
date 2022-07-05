a944b51018b21d2c83bdd720819deb7a77e744f4
Josiah <josiahelias11@gmail.com>
2022-07-05 14:38:53 -0700

    c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+
+
+/**
+
+ * print_times_table - Check main
+
+ * @n: An integer
+
+ * Description: Prints times table of n
+
+ * Return: 0 if n is greater than 15 or less than 0
+
+ */
+
+void print_times_table(int n)
+
+{
+
+	int i, j;
+
+
+
+	if (n > 0 && n < 15)
+
+	{
+
+		for (i = 0; i <= n; i++)
+
+		{
+
+			_putchar('0');
+
+			for (j = 1; j <= n; j++)
+
+				putformat(i * j);
+
+			_putchar('\n');
+
+		}
+
+	}
+
+}
