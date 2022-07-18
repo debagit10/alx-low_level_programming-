c2b912c7439b2c359a1fc5041ba7a50bd0d93718
Josiah Elias <josiahelias11@gmail.com>
2022-07-18 20:41:22 +0100

    c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,10 +2,10 @@
 #define NULL 0
 
 /**
-*   _strpbrk - return pointer to byte in s that matches a byte in accept
-*    @s: string to search
-*     @accept: target matches
-*      Return: pointer to index of string at first occurence
+* _strpbrk - return pointer to byte in s that matches a byte in accept
+* @s: string to search
+* @accept: target matches
+* Return: pointer to index of string at first occurence
 */
 
 char *_strpbrk(char *s, char *accept)
